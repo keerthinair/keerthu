f2d67986eff8f64b55f821b85e874811dbc92f24
keerthinair <keerthikrishna362@gmail.com>
2017-04-24 16:21:57 +0530

    Update largest.c

--- largest.c
+++ largest.c
@@ -3,7 +3,7 @@ int main()
 {
 double n1,n2,n3;
 printf("Enter three numbers:");'
-scnaf("%1f %1f %1f",&n1,&n2,&n3);
+scanf("%lf %lf %lf",&n1,&n2,&n3);
 if(n1>=n2 && n1>=n3)
 printf("%.2f is the largest number:",n1);
 if(n2>=n1 && n2>=n3)
